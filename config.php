90765922069c28bebfd8cc951486cbbc2d778900
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2020-07-23 00:56:24 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-DEFINE('USERNAME','braddock10');
-DEFINE('PASSWORD','Ph1d06I0eL__');
-DEFINE('DBNAME', 'braddock10');
+DEFINE('USERNAME','');
+DEFINE('PASSWORD','');
+DEFINE('DBNAME',  '');
